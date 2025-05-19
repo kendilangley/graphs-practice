43459dcbd56fe81afcaf82968286eec9187c329a
kendilangley <38380858+kendilangley@users.noreply.github.com>
2025-05-19 10:33:06 -0500

    line design

--- line.js
+++ line.js
@@ -1,14 +1,14 @@
-// Simulated version of Utils.months({ count: 7 }) since we aren't using Chart.js utils directly.
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const ctx = document.getElementById('lineChart').getContext('2d');
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October'];
 
 const data = {
   labels: labels,
   datasets: [{
     label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
+    data: [65, 59, 80, 81, 56, 55, 40, 12, 20, 15],
     fill: false,
     borderColor: 'rgb(75, 192, 192)',
-    tension: 0.1
+    tension: 0.1 //changes the ridgness of the lines
   }]
 };
 
@@ -17,5 +17,4 @@ const config = {
   data: data
 };
 
-const ctx = document.getElementById('lineChart').getContext('2d');
 new Chart(ctx, config);
