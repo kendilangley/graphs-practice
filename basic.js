ce43f1fe88f8ebad56dca223ca982992d4f67aaa
kendilangley <38380858+kendilangley@users.noreply.github.com>
2025-05-19 09:40:51 -0500

    basic design

--- basic.js
+++ basic.js
@@ -4,16 +4,27 @@ const basicChart = new Chart(ctx, {
   data: {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [{
+      axis: 'y',
+      barPercentage: 0.9,
       label: 'My First Dataset',
       data: [65, 59, 80, 81, 56, 55, 40],
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgb(255, 99, 132)',
+      fill: false,
+      backgroundColor: [
+        'rgba(201, 203, 207, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+
+      ],
+      hoverBackgroundColor: 'rgba(189, 195, 199)',
+      borderColor: 'rgba(0,0,0)',
       borderWidth: 1
     }]
   },
   options: {
-    scales: {
-      y: { beginAtZero: true }
-    }
+    indexAxis: 'y',
   }
 });
